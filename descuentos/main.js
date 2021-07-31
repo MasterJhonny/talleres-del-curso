5f5451d20de9b10bc9d36391f70631f2b7033c68
MasterJhonny <jhonnyeddy.22@gmail.com>
2021-07-30 21:42:06 -0400

    se anadio el codigo js

--- descuentos/main.js
+++ descuentos/main.js
@@ -1,9 +1,20 @@
-const precOrigin = 120
+const btn = document.getElementById('btn')
+const salida = document.getElementById('salida')
 const descuent = 18
-
 
 const precioConDesc = (precio, descuent) => {
     return (precio * (100 - descuent)) / 100
+}
+
+btn.onclick = () => {
+    let precio = document.getElementById('precio')
+    let valor = document.getElementById('valor')
+    let descuent = document.getElementById('descuento')
+    if(precio.value && descuent.value){
+        salida.innerText = `El precio con descuento es ${precioConDesc(parseInt(precio.value), parseInt(descuent.value))} ${valor.value}.`
+    } else {
+        salida.innerText = 'Llene todos los campos!'
+    }
 }
 
 
