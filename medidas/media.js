9383d4326cc48d5406396fe25dcd5c7069e38fa3
MasterJhonny <jhonnyeddy.22@gmail.com>
2021-08-04 02:35:55 -0400

    se anadio la carpeta de asset y modification a la medidas

--- medidas/media.js
+++ medidas/media.js
@@ -1,9 +1,50 @@
 // declaration of const 
-const salida1 = document.getElementById('salida1')
 const btn1 = document.getElementById('btn1')
-const salida2 = document.getElementById('salida2')
 const btn2 = document.getElementById('btn2')
-let lista1 = [200, 100 ,500, 430]
+const btn3 = document.getElementById('btn3')
+const salida1 = document.getElementById('salida1')
+const salida2 = document.getElementById('salida2')
+const salida3 = document.getElementById('salida3')
+
+const salida11 = document.getElementById('salida11')
+const salida22 = document.getElementById('salida22')
+const salida33 = document.getElementById('salida33')
+// declaration from the array
+let cadena = new Array()
+
+// funtio aditional
+// function for go number
+let arr = new Array()
+
+const goNumber = (id) => {
+    let area = document.getElementById(id)
+    if(area.value){
+        arr.push(parseInt(area.value))
+        salida11.innerText = `[${arr}]`
+        console.log(arr)
+        area.value = ""
+        btn1.disabled = false
+    } else {
+        alert("No")
+    }
+}
+
+// delete for Todo
+const deleteTodo = (id) => {
+    arr = new Array()
+    salida1.innerText = ''
+    salida11.innerText = ''
+}
+// function delete number
+const deleteNumber = () => {
+    if(arr.length != 0){
+        arr.pop()
+        salida11.innerText = `[${arr}]`
+    } else {
+        alert("No")
+        salida11.innerText = ""
+    }
+}
 
 // funtion for promenc
 const calcularPromedio = (lista) => {
@@ -23,33 +64,46 @@ const calcularMedia = (lista) => {
     }
 }
 
-// function for calculating if number is par 
-const esPar = (numero) => {
-    if(numero % 2 === 0) {
-        return true
-    } else {
-        return false
-    }
+// function for calculating Moda
+const calcularModa = (lista) => {
+    let valores = {}
+    lista.forEach(e => {
+        valores[e] = lista.filter(valor => valor === e).length
+    })
+    let array = Object.entries(valores).sort((a,b) => a[1] - b[1])
+    return parseInt(array[array.length - 1].shift())
 }
 
-// event from click
+// event from click btn1
 btn1.onclick = () => {
-    const area = document.getElementById('area1')
-    if(area.value) {
-        let lista = area.value.split(",")
-        let listaF = lista.map(e => parseInt(e))
-        salida1.innerText = `El Promedio de los valores ingresados es: ${calcularPromedio(listaF)}`
+    const area = arr
+    if(area.length != 0) {
+        salida1.innerText = `El Promedio de los valores ingresados es: ${calcularPromedio(area)}`
     } else {
         alert('No')
+        salida1.innerText = ""
+        salida11.innerText = ""
     }
 } 
-// event from click
+// event from click btn2
 btn2.onclick = () => {
     const area = document.getElementById('area2')
     if(area.value) {
         let lista = area.value.split(",")
         let listaF = lista.map(e => parseInt(e))
-        salida2.innerText = `La media de los valores ingresados es: ${calcularMedia(listaF)}`
+        salida2.innerText = `La Media de los valores ingresados es: ${calcularMedia(listaF)}`
+    } else {
+        alert('No')
+    }
+} 
+
+// event from click btn3
+btn3.onclick = () => {
+    const area = document.getElementById('area3')
+    if(area.value) {
+        let lista = area.value.split(",")
+        let listaF = lista.map(e => parseInt(e))
+        salida3.innerText = `La Moda de los valores ingresados es: ${calcularModa(listaF)}`
     } else {
         alert('No')
     }
--- medidas/promedio.js
+++ medidas/promedio.js
@@ -1,4 +1,4 @@
-let lista1 = [100, 200, 300, 500]
+let lista1 = [1,3,1,2,3,1,3,2,1,1,4]
 
 
 
@@ -14,4 +14,42 @@ let lista1 = [100, 200, 300, 500]
 const calcularPromedio = (lista) => {
     const suma = lista.reduce((acc, valor) => acc += valor)
     return suma/lista.length
+}
+
+let listaObjet = {}
+
+lista1.map(element => {
+    if(listaObjet[element]) {
+        listaObjet[element] += 1
+    } else {
+        listaObjet[element] = 1
+    }
+})
+
+// function for calculating if number is par 
+const esPar = (numero) => {
+    if(numero % 2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// function for calculating Moda
+const calularModa = (lista) => {
+    let valores = {}
+    lista.forEach(e => {
+        valores[e] = lista.filter(valor => valor === e).length
+    })
+    let array = Object.entries(valores).sort((a,b) => a[1] - b[1])
+    return parseInt(array[array.length - 1].shift())
+}
+
+// function for calculating Moda de otra manera
+
+function mode(arr){
+    return arr.sort((a,b) =>
+          arr.filter(v => v===a).length
+        - arr.filter(v => v===b).length
+    ).pop();
 }
